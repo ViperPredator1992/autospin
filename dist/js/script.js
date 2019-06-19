8058bf6cace7a69dddc99e2d9d2b655f5af4462a
AlexanderSemenov92 <42918575+AlexanderSemenov92@users.noreply.github.com>
2019-06-20 01:06:13 +0300

    Остался подвал

--- dist/js/script.js
+++ dist/js/script.js
@@ -34,6 +34,20 @@ $(document).ready(function () {
         nav: false,
         items: 1,
         dots: true
-    })
+    });
+
+    $(window).scroll(function () {
+        if ($(this).scrollTop() != 0) {
+            $('#toTop').fadeIn();
+        } else {
+            $('#toTop').fadeOut();
+        }
+    });
+
+    $('#toTop').click(function () {
+        $('body,html').animate({
+            scrollTop: 0
+        }, 1500);
+    });
     
 });
--- src/js/script.js
+++ src/js/script.js
@@ -35,5 +35,19 @@ $(document).ready(function () {
         items: 1,
         dots: true
     });
+
+    $(window).scroll(function () {
+        if ($(this).scrollTop() != 0) {
+            $('#toTop').fadeIn();
+        } else {
+            $('#toTop').fadeOut();
+        }
+    });
+
+    $('#toTop').click(function () {
+        $('body,html').animate({
+            scrollTop: 0
+        }, 1500);
+    });
     
 });
