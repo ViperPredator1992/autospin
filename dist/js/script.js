8ac2533b45a2533eccc9c351474316cc0abdf5b1
AlexanderSemenov92 <42918575+AlexanderSemenov92@users.noreply.github.com>
2019-06-19 12:37:40 +0300

    Шапка - готова

--- dist/js/script.js
+++ dist/js/script.js
@@ -1,5 +1,23 @@
 $(document).ready(function () {
    
-    
+    $('.burger').click(function () {
+        if ($('.menu-list').css('display') == 'none') {
+            $('.menu-list').slideDown();
+            $('.burger').addClass('burger-open');
+        } else {
+            $('.menu-list').slideUp();
+            $('.burger').removeClass('burger-open');
+        }
+    });
+
+    $('.navbar__text, .nav-icon-1').click(function () {
+        if ($('.navbar-list').css('display') == 'none') {
+            $('.navbar-list').slideDown();
+            $('.icon').addClass('open');
+        } else {
+            $('.navbar-list').slideUp();
+            $('.icon').removeClass('open');
+        }
+    });
     
 });
